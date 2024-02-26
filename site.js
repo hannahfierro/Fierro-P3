9a00037dbcc8f309b3f5d83a02fd88cd873235fe
hannahfierro <224HFierro@frhsd.com>
2024-02-26 08:32:56 -0500

    fixed html code and site.js

--- site.js
+++ site.js
@@ -20,89 +20,56 @@
 // FOR STEP 16, ADD THREE OF YOUR OWN FAVORITE MOVIES WITH METADATA TO THE END OF THE JSON FILE LIST
 */
 
-
 const vue_app = Vue.createApp({
-      // This automatically imports your movies.json file and puts it into
-      // the variable: movies
-      created () {
-          fetch('movies.json').then(response => response.json()).then(json => {
-              this.movies = json;
-          });
-      },
-      data() {
-          return {
-              // This holds your movies.json data.
-              movies: [],
-              // Additional variables for step 3
-              title: "IMDB + Hannah Fierro's Top 8 Movies", // Replace YourName with your actual name
-              owner: "Hannah Fierro", // Replace Your Name with your actual name
-              github: "https://github.com/hannahfierro/Fierro-P3" // Replace yourusername with your actual GitHub username
-          };
-      },
-      methods: {
-        getMonthText(dateArray){
-        if(dateArray[1] == 1){
-            return "January" + dateArray[2] + "," + dateArray[0]
-          }
-          if(dateArray[1] == 2){
-            return "February" + dateArray[2] + "," + dateArray[0]
-          }
-          if(dateArray[1] == 3){
-            return "March" + dateArray[2] + "," + dateArray[0]
-          }
-          if(dateArray[1] == 4){
-            return "April" + dateArray[2] + "," + dateArray[0]
-      }
-      if(dateArray[1] == 5){
-        return "May" + dateArray[2] + "," + dateArray[0]
-      }
-      if(dateArray[1] == 6){
-        return "June" + dateArray[2] + "," + dateArray[0]
-      }
-      if(dateArray[1] == 7){
-        return "July" + dateArray[2] + "," + dateArray[0]
-      }
-      if(dateArray[1] == 8){
-        return "August" + dateArray[2] + "," + dateArray[0]
-      }
-      if(dateArray[1] == 9){
-        return "September" + dateArray[2] + "," + dateArray[0]
-      }
-      if(dateArray[1] == 10){
-        return "October" + dateArray[2] + "," + dateArray[0]
-      }
-      if(dateArray[1] == 11){
-        return "November" + dateArray[2] + "," + dateArray[0]
-      }
-      if(dateArray[1] == 12){
-        return "December" + dateArray[2] + "," + dateArray[0]
-      }
-  
-    },
-  
-    like(index) {
-        this.movies[index].likes++
-    },
-    dislike(index){
-        this.movies[index].dislikes++
-    },
-    posterClick(index){
-        let length = this.movies[index].posters.length
-
-        this.movies[index].posterindex++
-        if(this.movies[index].posterindex >= length){
-            this.movies[index].posterindex = 0
-        }
-
+  // This automatically imports your movies.json file and puts it into
+  //   the variable: movies
+  created () {
+        fetch('movies.json').then(response => response.json()).then(json => {
+              this.movies = json
+        })
+  },
+  data() {
+    return {
+        // This holds your movies.json data.
+        movies: [],
+        /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+        trueMonths: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        Title: "IMDB + Hannah's Top 8 Movies",
+        owner: "Hannah Fierro",
+        github: "https://github.com/hannahfierro/p3",
+     
+  }
 },
-timeText(minutes){
-    let hr = Math.trunc(minutes/60)
-    let min = minutes - 60*hr
-    return hr + "h" +  min + "m"
-}
+  methods: {
+        /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+        getMonthText(dateArray) {
+            var year = dateArray[0];
+            var month = this.trueMonths[dateArray[1] - 1];
+            var day = dateArray[2];
+            return month + ' ' + day + ', ' + year;
+        },
 
-}
-
+        like(index){
+            this.movies[index].likes++;
+        },
+        dislike(index){
+            this.movies[index].dislikes++;
+        },
+        posterClick(index){
+            if(this.movies[index].posterindex < this.movies[index].posters.length) {
+                  this.movies[index].posterindex++;
+            };
+            if(this.movies[index].posterindex >= this.movies[index].posters.length) {
+                  this.movies[index].posterindex = 0;
+            }
+        },
+        timeText(minutes){
+            var hours = Math.floor(minutes/60);
+            var minute = minutes % 60;
+            return hours + ' hours ' + minute + ' minutes';
+        },
+  }
 })
-  vue_app.mount("#vue_app");
+
+vue_app.mount("#vue_app")
   
